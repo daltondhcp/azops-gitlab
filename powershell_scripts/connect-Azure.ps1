101f72daa19216590f9663755aefabb8aa753b4e
Johan Dahlbom <johan@dahlbom.eu>
2022-11-03 09:33:22 +0000

    update

--- powershell_scripts/connect-Azure.ps1
+++
@@ -1,2 +0,0 @@
-$credential = New-Object PSCredential -ArgumentList $env:ARM_CLIENT_ID, (ConvertTo-SecureString -String $env:ARM_CLIENT_SECRET -AsPlainText -Force)
-Connect-AzAccount -TenantId $env:ARM_TENANT_ID -ServicePrincipal -Credential $credential -SubscriptionId $env:ARM_SUBSCRIPTION_ID
--- powershell_scripts/sync.ps1
+++
@@ -1,3 +0,0 @@
-Import-PSFConfig -Path settings.json -Schema MetaJson -EnableException
-Invoke-AzOpsPull -Rebuild
-Get-Job | Remove-Job -Force
